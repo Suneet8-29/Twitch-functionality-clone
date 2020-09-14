49dd10b7e98723eeca08233a38bab0a2fa822204
Suneet Jena <suneetjena@Suneets-MacBook-Air.local>
2020-09-14 08:40:01 +0530

    obs issue to be resolved

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,13 +1,38 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, createRef } from 'react'
 import { connect } from 'react-redux'
 import {fetchStream} from '../../actions'
-
+import flv from 'flv.js'
 const StreamShow = (props) => {
 
+    const videoRef = createRef();
+    var flvPlayer;
     useEffect(() => {
         props.fetchStream(props.match.params.id)
+        buildPlayer();
+
+        return () => {
+            flvPlayer.destroy();
+        }
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
+    useEffect(() => {
+         buildPlayer();
+     // eslint-disable-next-line react-hooks/exhaustive-deps
+     })
+
+    const buildPlayer = () => {
+        if (flvPlayer || !props.stream) {
+            return;
+        }
+        flvPlayer = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/${props.match.params.id}.flv`
+        });
+        flvPlayer.attachMediaElement(videoRef.current);
+        flvPlayer.load();
+        //flvPlayer.play();
+    }
 
     const renderStream = (stream) => {
         if (!stream) {
@@ -16,6 +41,7 @@ const StreamShow = (props) => {
 
         return (
             <div>
+                <video ref = {videoRef} style = {{width : '100%'}} controls ></video>
                 <h1>{stream.title}</h1>
                 <h5>{stream.description}</h5>
             </div>
